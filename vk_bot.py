bdf491e334a9450fc17f1f5c3678393c8b6e7889
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-07-09 13:18:15 +0500

    vk echo bot added

---
+++ vk_bot.py
@@ -0,0 +1,38 @@
+import random
+import os
+
+import vk_api as vk
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.longpoll import VkLongPoll, VkEventType
+from dotenv import load_dotenv
+
+
+def echo(event, vk_api):
+    keyboard = VkKeyboard(one_time=True)
+
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+
+    keyboard.add_line()  # Переход на вторую строку
+    keyboard.add_button('Мой счёт', color=VkKeyboardColor.SECONDARY)
+
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000),
+        keyboard=keyboard.get_keyboard()
+    )
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    vk_token = os.getenv('VK_GROUP_TOKEN')
+
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
